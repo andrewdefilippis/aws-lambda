ab997294d846aa92cb85388ff6b6d525e5150fd8
Andrew R. DeFilippis <andrewdefilippis@gmail.com>
2016-12-15 02:47:13 -0800

    Commit the Echo Python Function.

---
+++ Functions/echo-python/lambda_function.py
@@ -0,0 +1,34 @@
+"""Created By: Andrew Ryan DeFilippis"""
+
+print('Lambda cold-start...')
+
+from json import dumps, loads
+
+
+def lambda_handler(event, context):
+    print('LOG RequestId: {}\tResponse:\n\n{}'.format(
+        context.aws_request_id,
+        dumps(event, indent=4)
+    ))
+
+    return event
+
+
+# Comment or remove everything below before deploying to Lambda.
+def local_testing():
+    import context
+
+    with open('event.json', 'r') as f:
+        event = loads(f.read())
+
+    print("Event:\n\n{}\n\nFunction Output:\n".format(
+        dumps(
+            event,
+            indent=4
+        )
+    ))
+
+    lambda_handler(event, context)
+
+
+local_testing()
