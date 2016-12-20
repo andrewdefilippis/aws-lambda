d8e2e6ed37d5096bcac90ac1b7ae602ec0d23a14
Andrew R. DeFilippis <andrewdefilippis@gmail.com>
2016-12-19 20:36:28 -0800

    Add documentation, and modify default values

--- Functions/ses_inbound_forwarder-python/lambda_function.py
+++ Functions/ses_inbound_forwarder-python/lambda_function.py
@@ -11,8 +11,8 @@ from email.mime.multipart import MIMEMultipart
 
 
 # Disable 'testing_locally' when deploying to AWS Lambda
-testing_locally = False
-verbose = False
+testing_locally = True
+verbose = True
 
 
 # Define the S3 bucket name and key prefix (Ex. 'users/' *Remember to include the trailing slash)
@@ -39,18 +39,44 @@ forward_statement = (
 
 
 class CWLogs(object):
+    """Define the structure of log events to match all other CloudWatch Log Events logged by AWS Lambda.
+    """
+
     def __init__(self, context):
+        """Define the instance of the context object.
+
+        :param context: Lambda context object
+        """
+
         self.context = context
 
     def event(self, message, event_prefix='LOG'):
+        # type: (any, str) -> None
+        """Print an event into the CloudWatch Logs stream for the Function's invocation.
+
+        :param message: The information to be logged (required)
+        :param event_prefix: The prefix that appears before the 'RequestId' (default 'LOG')
+        :return:
+        """
+
         print('{} RequestId: {}\t{}'.format(
             event_prefix,
             self.context.aws_request_id,
             message
         ))
 
+        return None
+
 
 def lambda_handler(event, context):
+    """AWS Lambda executes the 'lambda_handler' function on invocation.
+
+    :param event: Ingested JSON event object provided at invocation
+    :param context: Lambda context object, containing information specific to the invocation and Function
+    :return: Final response to AWS Lambda, and passed to the invoker if the invocation type is RequestResponse
+    """
+
+    # Instantiate our CloudWatch logging class
     log = CWLogs(context)
 
     if verbose is True:
@@ -58,33 +84,40 @@ def lambda_handler(event, context):
 
     log.event('Creating S3 resource and SES client instances')
 
+    # Instantiate the S3 resource and SES client classes
     s3_resource = resource('s3', region_name=s3_region)
     ses_client = client('ses', region_name=ses_region)
 
+    # There *should* only be one (1) record
     for record in event['Records']:
         log.event('Parsing the event record')
 
+        # Assign values from the record to variables
         recipient = record['ses']['receipt']['recipients'][0]
         username = recipient.split("@")[0]
         msg_id = record['ses']['mail']['messageId']
 
+        # Build the S3 object key from the prefix including the username and the message ID
         msg_key_prefix = s3_key_prefix.format(username)
         s3_msg_key = msg_key_prefix + msg_id
 
         log.event('Retrieving email message')
 
+        # Get the message body from the object retrieved from S3
         s3_obj = s3_resource.Object(ses_inbound_s3_bucket, s3_msg_key)
         s3_msg_body = s3_obj.get()["Body"].read()
         msg_body = message_from_string(s3_msg_body)
 
         log.event('Generating new email message.')
 
+        # Create the new email message to be forwarded
         new_email = MIMEBase("multipart", "mixed")
 
         new_email['Subject'] = 'Fwd: {}'.format(msg_body['subject'])
         new_email['From'] = recipient
         new_email['To'] = dest_email_addr
 
+        # Add the 'forwarded' heading to the new message body
         new_email.attach(MIMEText(forward_statement.format(
             msg_body['from'],
             msg_body['date'],
@@ -92,6 +125,7 @@ def lambda_handler(event, context):
             msg_body['to']
         )))
 
+        # Add all the parts from the original message
         if msg_body.is_multipart():
             inner_msg = MIMEMultipart("alternative")
             for part in msg_body.get_payload():
@@ -105,6 +139,7 @@ def lambda_handler(event, context):
 
         log.event('Sending new email message to SES')
 
+        # SES should accept the new message and send it to the recipient
         ses_response = ses_client.send_raw_email(
             Source=new_email['From'],
             Destinations=[new_email['To']],
@@ -119,6 +154,9 @@ def lambda_handler(event, context):
 
 
 def local_test():
+    """Testing on a local development machine (outside of AWS Lambda) is made possible by...
+    """
+
     import context
 
     with open('event.json', 'r') as f:
